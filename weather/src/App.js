22425f94cdf58a77964b84f0d3ef022e91b546f5
acong <acong@example.com>
2018-10-12 07:23:39 +0700

    Tweaked daily data accuracy and used .foreach instead of .map

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,113 +1,55 @@
 	import React, { Component } from 'react';
 	import axios from 'axios';
 	import './App.css';
-	// &appid=271da6b323b05ebaf2b4aaa0f3378f89
+
 	class App extends Component {
-	state={cityName:'Select Your City', data:[], dataTanggal:[], dataTemp:[], dataDelta:[], dataTabel:[]}
+	state=	{ 	
+				cityName:'Select Your City', 
+				data:[]
+			}
 
 	getData=()=>{
-		if(this.refs.City.value!==''){
-			axios.get(`https://api.openweathermap.org/data/2.5/forecast?id=${this.refs.City.value}&units=metric&appid=271da6b323b05ebaf2b4aaa0f3378f89`)
+		if(this.refs.City.value !== ''){
+			axios.get(`	https://api.openweathermap.org/data/2.5/forecast?
+						id=${this.refs.City.value}&
+						units=metric&
+						appid=271da6b323b05ebaf2b4aaa0f3378f89`)
 			.then(ok=>{
-				this.setState({cityName:ok.data.city.name, data: ok.data.list})
-				console.log(this.state.data)
+				this.setState({ cityName:ok.data.city.name, data: ok.data.list })
+			}).catch(error=>{
+				console.log(error)
 			})
 		}
-		// if(this.state.data !== []){
-		// 	console.log('masuk')
-		// 	this.state.data.map(data=>{
-		// 		console.log(data)
-		// 	})
-		// }
-		
 	}
-
-	renderAvgTemp=()=>{
-		var totalTemp = 0;
+	
+	renderAvgSuhu=()=>{
+		var totalSuhu = 0;
 		if(this.state.data.length > 0){
-			this.state.data.map(data=>{
-				totalTemp += data.main.temp
+			this.state.data.forEach(data=>{
+				totalSuhu += data.main.temp
 			})
 		}
-		var rerataTemp = (totalTemp/this.state.data.length)
-		return rerataTemp.toFixed(2)
+		var avgSuhu = (totalSuhu/this.state.data.length)
+		if(isNaN(avgSuhu)){
+			return '-'
+		}
+		return avgSuhu.toFixed(2)
 	}
 
-	renderAvgDiff=()=>{
-		var totalDeltaTemp = 0;
+	renderAvgDelta=()=>{
+		var totalDeltaSuhu = 0;
 		if(this.state.data.length > 0){
-			this.state.data.map(data=>{
-				totalDeltaTemp += data.main.temp_kf
+			this.state.data.forEach(data=>{
+				totalDeltaSuhu += data.main.temp_kf
 			})
 		}
-		var rerataDeltaTemp = (totalDeltaTemp/this.state.data.length)
-		return rerataDeltaTemp.toFixed(2);
+		var avgDeltaSuhu = (totalDeltaSuhu/this.state.data.length)
+		console.log()
+		if(isNaN(avgDeltaSuhu)){
+			return '-'
+		}
+		return avgDeltaSuhu.toFixed(2);
 	}
-
-	// isiArrayTabel = () => {
-	// 	console.log('terpanggil')
-	// 	var dataTabel = []
-	// 	if(this.state.data.length !== 0){
-	// 		for(var i=0; i<40; i+=8){
-	// 			var getTanggal = new Date((this.state.data.list[i].dt)*1000)
-	// 			this.setState({
-	// 				dataTanggal:`${getTanggal.getFullYear()}-${getTanggal.getMonth()}-${getTanggal.getDate()}`,
-	// 				dataTemp:this.state.data.list[i].main.temp,
-	// 				dataDelta: this.state.data.list[i].main.temp_kf
-	// 			})
-	// 			if(i==0){
-	// 				setTimeout(this.setState({
-	// 					dataTabel:
-	// 						[
-	// 							this.state.dataTanggal, 
-	// 							this.state.dataTemp, 
-	// 							this.state.dataDelta
-	// 						]
-	// 				}),300)
-	// 			}
-	// 			setTimeout(this.setState({
-	// 				dataTabel:
-	// 					[...dataTabel, 
-	// 						[
-	// 							this.state.dataTanggal, 
-	// 							this.state.dataTemp, 
-	// 							this.state.dataDelta
-	// 						]
-	// 					]
-	// 				}), 300)
-				
-				// var tanggal = []
-				// var suhu = []
-				// var deltaSuhu = []
-				// var dataHarian = []
-				// this.setState({dataTabel: [...dataTabel, `${getTanggal.getFullYear()}-${getTanggal.getMonth()}-${getTanggal.getDate()}`,]})
-				// dataHarian.push(`${getTanggal.getFullYear()}-${getTanggal.getMonth()}-${getTanggal.getDate()}`)
-				// dataHarian.push(this.state.data.list[i].main.temp)
-				// dataHarian.push(this.state.data.list[i].main.temp_kf)
-				// console.log(dataHarian)
-				// dataTabel.push(dataHarian)
-				// console.log(dataTabel)
-	// 		}
-	// 		if(this.state.dataTabel.length>0){
-	// 			return this.state.dataTabel.map(data=>{
-					
-	// 			})
-	// 		}
-	// 	}
-	// }
-	// 	renderTanggal=()=>{
-	// 		return
-	// 	}
-	// 		return(
-	// 			<tr>
-	// 				<td style={{textAlign:"center"}}>{tanggal.getFullYear()}-{tanggal.getMonth()}-{tanggal.getDate()}</td>
-	// 				{/* <td style={{textAlign:"center"}}>{this.state.data.list[i].dt_txt.slice(0, 10)}</td> */}
-	// 				<td style={{textAlign:"center"}}>{this.state.data.list[i].main.temp}C</td>
-	// 				<td style={{textAlign:"center"}}>{this.state.data.list[i].main.temp_kf}C</td>
-	// 			</tr>
-	// 		)
-	// 	}	
-	//   }
 
 	createTable = () => {
 		var table = [];
@@ -115,16 +57,73 @@
 		if(this.state.data.length !== 0){
 			for(var i=0; i<40; i+=8){
 				let child = [];
-				console.log(i)
 				var getTanggal = new Date((this.state.data[i].dt)*1000)
+
 				for(let j=0; j<1; j++){
-					child.push(
-						<td>{getTanggal.getFullYear()}-{getTanggal.getMonth()}-{getTanggal.getDate()}</td>,
-						<td>{this.state.data[i].main.temp}C</td>,
-						<td>{this.state.data[i].main.temp_kf}C</td>
-					)
+					// API returned only 39 data as of 7.10 12-Oct-18 so an if else statement is used
+
+					if(this.state.data[i+7] !== undefined){
+						// variabel untuk mendapatkan rerata suhu harian
+						var suhu = 	((
+										this.state.data[i].main.temp + 
+										this.state.data[i+1].main.temp +
+										this.state.data[i+2].main.temp +
+										this.state.data[i+3].main.temp +
+										this.state.data[i+4].main.temp +
+										this.state.data[i+5].main.temp +
+										this.state.data[i+6].main.temp +
+										this.state.data[i+7].main.temp 
+									)/8).toFixed(2)
+
+						// variabel untuk mendapatkan rerata delta suhu harian
+						var deltaSuhu = ((
+											this.state.data[i].main.temp_kf + 
+											this.state.data[i+1].main.temp_kf +
+											this.state.data[i+2].main.temp_kf +
+											this.state.data[i+3].main.temp_kf +
+											this.state.data[i+4].main.temp_kf +
+											this.state.data[i+5].main.temp_kf +
+											this.state.data[i+6].main.temp_kf +
+											this.state.data[i+7].main.temp_kf 
+										)/8).toFixed(2)
+
+						child.push(
+							<td key={(i+1)*1.1} >{getTanggal.getFullYear()}-{getTanggal.getMonth()}-{getTanggal.getDate()}</td>,
+							<td key={(i+1)*1.2}>{suhu}C</td>,
+							<td key={(i+1)*1.3}>{deltaSuhu}C</td>
+						)
+					}
+					// variabel untuk mendapatkan rerata suhu harian
+					else{
+						var suhu2 = 	((
+										this.state.data[i].main.temp + 
+										this.state.data[i+1].main.temp +
+										this.state.data[i+2].main.temp +
+										this.state.data[i+3].main.temp +
+										this.state.data[i+4].main.temp +
+										this.state.data[i+5].main.temp +
+										this.state.data[i+6].main.temp 
+									)/8).toFixed(2)
+
+						// variabel untuk mendapatkan rerata delta suhu harian
+						var deltaSuhu2 = ((
+											this.state.data[i].main.temp_kf + 
+											this.state.data[i+1].main.temp_kf +
+											this.state.data[i+2].main.temp_kf +
+											this.state.data[i+3].main.temp_kf +
+											this.state.data[i+4].main.temp_kf +
+											this.state.data[i+5].main.temp_kf +
+											this.state.data[i+6].main.temp_kf 
+										)/8).toFixed(2)
+
+						child.push(
+							<td key={(i+1)*1.1} >{getTanggal.getFullYear()}-{getTanggal.getMonth()}-{getTanggal.getDate()}</td>,
+							<td key={(i+1)*1.2}>{suhu2}C</td>,
+							<td key={(i+1)*1.3}>{deltaSuhu2}C</td>
+						)
+					}
 				}
-				table.push(<tr>{child}</tr>)
+				table.push(<tr key={i}>{child}</tr>)
 			}
 		}
 		return table
@@ -134,13 +133,17 @@
 		return (
 		<div className="App">
 			<h1>Histori Cuaca 5 hari</h1>
-			City :<select ref='City'>
+			Pilih kota dan klik Go!
+			<br></br>
+			<br></br>
+			Kota :<select ref='City'>
 					<option value=''> - - - - -</option>
 					<option value='1642911'>Jakarta</option>
 					<option value='1880252'>Singapura</option>
 					<option value='1609350'>Bangkok</option>
 				</select>
 			<input type='button' onClick={this.getData} value='GO'/>
+			<br></br>
 			<br></br>
 			<table className='tftable'>
 				<thead>
@@ -154,9 +157,11 @@
 					{this.createTable()}
 				</tbody>
 				<tfoot>
-					<th style={{textAlign:"center"}}>Rata-rata</th>
-					<th style={{textAlign:"center"}}>{this.renderAvgTemp()}</th>
-					<th style={{textAlign:"center"}}>{this.renderAvgDiff()}</th>
+					<tr>
+						<th style={{textAlign:"center"}}>Rata-rata</th>
+						<th style={{textAlign:"center"}}>{this.renderAvgSuhu()}</th>
+						<th style={{textAlign:"center"}}>{this.renderAvgDelta()}</th>
+					</tr>
 				</tfoot>
 			</table>
 		</div>
